e26ecc4410d0d7fd59238fc520d8a0988fb5f9ed
ghoyalhimant-debug <ghoyalhimant@gmail.com>
2025-12-21 12:55:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-2.5-flash')
 except:
     st.error("API Key not found. Please set it in Streamlit Secrets.")
 
